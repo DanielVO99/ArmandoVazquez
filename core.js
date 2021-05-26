9dd5fa04b9d3de4a063c5f2a8f7ea2e2ad263364
saleth28 <jmmr12010@gmail.com>
2021-05-26 14:48:15 -0500

    Añadir la presentacion de las imagenes de referencia

---
+++ core.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById('canvas'),
+context = canvas.getContext('2d');
+
+
+context.fillStyle = '#000000'
+context.font = 'bold 20px Trebuchet MS'
+context.fillText("TRICERACOP", 250, 20)
